1985fa8852ee9beefe5d1bb4090566f4e46baf97
Eduardo <eduardo.942712@gmail.com>
2021-03-18 12:34:28 -0600

    created components where we will render basic charts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/bar', pathMatch: 'full' },
+    { path: 'bar', component: BarComponent },
+    { path: 'pie', component: PieComponent },
+    { path: 'scatter', component: ScatterComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
